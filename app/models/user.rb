a95d38f262888ce5819a6c98b8872776795c08c7
WilfredKesh <wilfredgish5@gmail.com>
2016-07-14 23:21:17 -0400

    added friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username
   validates_uniqueness_of :username
+
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+
+  def request_friendship(friend)
+  	self.friendships.create(friend: friend)
+
+  end
+
+  def pending_incoming_requests
+  	self.inverse_friendships.where(state: "pending")
+  end
+
+  def pending_outgoing_requests
+  	self.friendships.where(state: "pending")
+  end
+
+  def active_friends
+  	self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:user)
+  end
 end
